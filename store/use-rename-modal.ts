f29a783dbf0b34270aba46028ad51b913dbdde02
Basilisk <prith.tiu13@gmail.com>
2024-03-16 22:16:38 +0530

    bleh

--- store/use-rename-modal.ts
+++ store/use-rename-modal.ts
@@ -7,19 +7,17 @@ interface IRenameModal {
   initialValues: typeof defaultValues;
   onOpen: (id: string, title: string) => void;
   onClose: () => void;
-}
+};
 
 export const useRenameModal = create<IRenameModal>((set) => ({
   isOpen: false,
-  onOpen: (id, title) =>
-    set({
-      isOpen: true,
-      initialValues: { id, title },
-    }),
-  onClose: () =>
-    set({
-      isOpen: false,
-      initialValues: defaultValues,
-    }),
+  onOpen: (id, title) => set({
+    isOpen: true,
+    initialValues: { id, title },
+  }),
+  onClose: () => set({
+    isOpen: false,
+    initialValues: defaultValues,
+  }),
   initialValues: defaultValues,
 }));
