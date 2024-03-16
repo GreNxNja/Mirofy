e47bd84ce492dc9857ccd608250ccca3fb800f09
Basilisk <prith.tiu13@gmail.com>
2024-03-16 22:17:27 +0530

    bruh

--- app/(dashboard)/_components/new-board-button.tsx
+++ app/(dashboard)/_components/new-board-button.tsx
@@ -3,7 +3,6 @@
 import { toast } from "sonner";
 import { Plus } from "lucide-react";
 import { useRouter } from "next/navigation";
-
 import { cn } from "@/lib/utils";
 import { api } from "@/convex/_generated/api";
 import { useApiMutation } from "@/hooks/use-api-mutation";
